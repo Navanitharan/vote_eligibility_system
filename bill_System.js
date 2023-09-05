9321554dccea7fad7f4df087ff589ede5f934f82
navani505 <102136576+navani505@users.noreply.github.com>
2023-09-05 13:45:01 +0530

    Delete bill_System.js

--- bill_System.js
+++
@@ -1,6 +0,0 @@
-function split () {
-let amount = document.getElementById("amount").value
-let person = document.getElementById("person").value
-let result = amount/person
-document.getElementById("title").innerText=result.toFixed(2)+" for each 🤑"
-}
